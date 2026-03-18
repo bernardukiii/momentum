49ba4f01bf9c67e635a21de1473843978d4a120b
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-03-18 16:23:28 +0100

    chore: adding the dashboard route and page to redirect after the sign up or login

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from "react"
+
+const Dashboard: React.FC = () => {
+  return (
+    <main className=''>
+        <div className="h-screen flex flex-col justify-around items-center bg-linear-to-t from-momentum-primary-purple via-momentum-bg-soft via-90% to-white">
+          <section className="w-full flex flex-col justify-between items-center">
+            DASHBOARD PAGE
+          </section>
+        </div>
+    </main>
+  )
+}
+
+export default Dashboard
