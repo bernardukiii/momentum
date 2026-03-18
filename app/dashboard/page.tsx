4cbe7acf0d8559b63a637a7cae85c4395a66f9da
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-03-18 16:34:39 +0100

    chore: add sign out button to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,6 +8,12 @@ const Dashboard: React.FC = () => {
         <div className="h-screen flex flex-col justify-around items-center bg-linear-to-t from-momentum-primary-purple via-momentum-bg-soft via-90% to-white">
           <section className="w-full flex flex-col justify-between items-center">
             DASHBOARD PAGE
+
+            <button 
+              className="w-full h-12 bg-momentum-primary-purple hover:bg-momentum-primary-indigo-hover text-white font-bold rounded-lg shadow-lg shadow-momentum-primary-purple/20 transition-all active:scale-[0.98] disabled:opacity-70"
+              >
+              SIGN OUT
+            </button>
           </section>
         </div>
     </main>
