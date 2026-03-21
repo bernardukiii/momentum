2614bff9020b2a67e02c1c20b57d28abcbcf77d6
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-03-21 09:20:36 +0100

    chore: added the mini sctructure for the strava card

--- app/dashboard/components/strava/StravaSummary.tsx
+++ app/dashboard/components/strava/StravaSummary.tsx
@@ -13,11 +13,18 @@ interface StravaSummaryProps {
 const StravaSummary: React.FC<StravaSummaryProps> = ({ distance, unit, activitiesThisWeek }) => {
   return (
     <div className="flex flex-col justify-center items-center h-full space-y-3 text-black">
-        <h1>Insight summary</h1>
+        <h1 className='text-lg font-bold text-black'>Insight summary</h1>
         {/* Toggle switch */}
         <div className='w-full flex justify-end items-center'>
             <MomentumToggle />
         </div>
+
+        <section className='w-full h-full flex justify-center items-center'>
+            {/* LEFT SIDE */}
+            <div className='w-full h-full bg-green-50 flex justify-center items-center'>Mini line chart</div>
+            {/* RIGHT SIDE */}
+            <div className='w-full h-full bg-red-50 flex justify-center items-center'>Insights</div>
+        </section>
     </div>
   )
 }
