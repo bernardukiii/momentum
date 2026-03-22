b8c4911e44336654cc05de14a7cafb63ed2012c6
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-03-22 11:30:22 +0100

    chore: updating toggle to take 3 types of activities + adding png for walking type

--- components/momentum/MomentumToggle.tsx
+++ components/momentum/MomentumToggle.tsx
@@ -4,23 +4,27 @@ import React from 'react'
 import Image from 'next/image'
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
-interface MomentumToggle {
-  onTypeChange: (value: 'bike' | 'run') => void
+// Define the types to match your utility-functions.ts
+type ActivityType = 'Ride' | 'Run' | 'Walk'
+
+interface MomentumToggleProps {
+  // This must match exactly what you pass from the parent
+  onTypeChange: (value: ActivityType) => void
 }
 
-export const MomentumToggle: React.FC<MomentumToggle> = ({ onTypeChange }) => {
+export const MomentumToggle: React.FC<MomentumToggleProps> = ({ onTypeChange }) => {
   return (
     <div className="w-full flex justify-end">
       <Tabs 
-        defaultValue="bike" 
-        onValueChange={(val) => onTypeChange(val as 'bike' | 'run')}
-        className="w-28"
+        defaultValue="Ride" 
+        onValueChange={(val) => onTypeChange(val as ActivityType)}
+        className="w-42" // Widened slightly to fit 3 icons
       >
-        <TabsList className="grid w-full grid-cols-2 h-10 bg-gray-100 rounded-xl p-1 border border-gray-200">
+        <TabsList className="grid w-full grid-cols-3 h-10 bg-gray-100 rounded-xl p-1 border border-gray-200">
           
           {/* Bike Tab */}
           <TabsTrigger 
-            value="bike" 
+            value="Ride" 
             className="group rounded-lg transition-all data-[state=active]:bg-white data-[state=active]:shadow-sm"
           >
             <Image 
@@ -34,7 +38,7 @@ export const MomentumToggle: React.FC<MomentumToggle> = ({ onTypeChange }) => {
 
           {/* Run Tab */}
           <TabsTrigger 
-            value="run" 
+            value="Run" 
             className="group rounded-lg transition-all data-[state=active]:bg-white data-[state=active]:shadow-sm"
           >
             <Image 
@@ -42,6 +46,20 @@ export const MomentumToggle: React.FC<MomentumToggle> = ({ onTypeChange }) => {
               width={18} 
               height={18} 
               alt="run" 
+              className="opacity-40 group-data-[state=active]:opacity-100 transition-opacity" 
+            />
+          </TabsTrigger>
+
+          {/* Walk Tab */}
+          <TabsTrigger 
+            value="Walk" 
+            className="group rounded-lg transition-all data-[state=active]:bg-white data-[state=active]:shadow-sm"
+          >
+             <Image 
+              src="/walking-person.png"
+              width={18} 
+              height={18} 
+              alt="walk" 
               className="opacity-40 group-data-[state=active]:opacity-100 transition-opacity" 
             />
           </TabsTrigger>
