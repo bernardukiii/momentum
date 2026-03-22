9908cf71ac5e0e14d955295e7e1891a00a0f3716
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-03-22 10:41:54 +0100

    chore: adding a bit of styling just to make it readable

--- app/dashboard/components/strava/StravaSummary.tsx
+++ app/dashboard/components/strava/StravaSummary.tsx
@@ -50,60 +50,60 @@ const StravaSummary: React.FC = ({ }) => {
             <div className='w-full h-full bg-green-50 flex justify-center items-center'>Mini line chart</div>
             {/* RIGHT SIDE */}
             <div className='w-full h-full bg-red-50 flex justify-center items-center'>
-              <section className='flex flex-col justify-center items-center'>
+              <section className='w-full flex flex-col justify-center items-center'>
                 {/* BIKE */}
                 <div className='flex flex-col justify-center items-center'>
-                  <h3>Bike rides</h3>
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly km's</h4>
+                  <h3 className='text-black text-lg underline font-bold' >Bike rides</h3>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly km's</h4>
                     <span>{weeklyKmBike}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly time spent</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly time spent</h4>
                     <span>{weeklyMinsBike}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly calories burnt</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly calories burnt</h4>
                     <span>{weeklyCalsBike}</span>
                   </div>
                 </div>
 
                 {/* Run */}
                 <div className='flex flex-col justify-center items-center'>
-                  <h3>Runs</h3>
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly km's</h4>
+                  <h3 className='text-black text-lg underline font-bold' >Runs</h3>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly km's</h4>
                     <span>{weeklyKmRun}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly time spent</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly time spent</h4>
                     <span>{weeklyMinsRun}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly calories burnt</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly calories burnt</h4>
                     <span>{weeklyCalsRun}</span>
                   </div>
                 </div>
 
                 {/* Walk */}
                 <div className='flex flex-col justify-center items-center'>
-                  <h3>Walks</h3>
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly km's</h4>
+                  <h3 className='text-black text-lg underline font-bold' >Walks</h3>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly km's</h4>
                     <span>{weeklyKmWalk}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly time spent</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly time spent</h4>
                     <span>{weeklyMinsWalk}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly calories burnt</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly calories burnt</h4>
                     <span>{weeklyCalsWalk}</span>
                   </div>
                 </div>
@@ -111,19 +111,19 @@ const StravaSummary: React.FC = ({ }) => {
 
                 {/* Total */}
                 <div className='flex flex-col justify-center items-center'>
-                  <h3>Total</h3>
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly km's</h4>
+                  <h3 className='text-black text-lg underline font-bold' >Total</h3>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly km's</h4>
                     <span>{weeklyKmTotal}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly time spent</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly time spent</h4>
                     <span>{weeklyMinsTotal}</span>
                   </div>
 
-                  <div className='flex justify-between items-center'>
-                    <h4>Weekly calories burnt</h4>
+                  <div className='flex justify-between items-center w-full'>
+                    <h4 className='font-semibold'>Weekly calories burnt</h4>
                     <span>{weeklyCalsTotal}</span>
                   </div>
                 </div>
