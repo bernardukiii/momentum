b96d81a3466e7bae4f4d667f024ab9f23f70022e
Bernardo Ferrari <ber.97ferrari@gmail.com>
2026-03-28 11:47:27 +0100

    styling: editing the right section of the strava activities card

--- app/dashboard/components/strava/ActivitySnippet.tsx
+++ app/dashboard/components/strava/ActivitySnippet.tsx
@@ -16,22 +16,22 @@ const ActivitySnippet: React.FC<ActivitySnippetProps> = ({
   weeklyCalories 
 }) => {
   return (
-    <div className='flex flex-col justify-center items-center w-full p-4'>
-      <h3 className='text-black text-lg underline font-bold mb-2'>{activityTitle}</h3>
+    <div className='flex flex-col justify-center items-center w-fit p-4'>
+      <h3 className='text-black text-lg underline mb-2'>{activityTitle}</h3>
       
       <div className='flex justify-between items-center w-full'>
-        <h4 className='font-semibold text-sm'>Weekly Km's</h4>
-        <span className="font-bold text-orange-600">{weeklyKms} km</span>
+        <h4 className='text-sm uppercase mr-4'>Weekly Km's</h4>
+        <span className="font-bold text-momentum-midnight-indigo">{weeklyKms} km</span>
       </div>
 
       <div className='flex justify-between items-center w-full'>
-        <h4 className='font-semibold text-sm'>Weekly time</h4>
-        <span className="font-bold text-orange-600">{weeklyTime} min</span>
+        <h4 className=' text-sm uppercase mr-4'>Weekly time</h4>
+        <span className="font-bold text-momentum-midnight-indigo">{weeklyTime} min</span>
       </div>
 
       <div className='flex justify-between items-center w-full'>
-        <h4 className='font-semibold text-sm'>Daily Avg Cal</h4>
-        <span className="font-bold text-orange-600">{weeklyCalories} kcal</span>
+        <h4 className=' text-sm uppercase mr-4'>Daily Avg Cal</h4>
+        <span className="font-bold text-momentum-midnight-indigo">{weeklyCalories} kcal</span>
       </div>
     </div> 
   )
